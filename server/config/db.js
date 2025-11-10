d87c155da8d84391e6fc9e16bb7299238a7a347d
Sagor96 <59876556+AshrafulAlam96@users.noreply.github.com>
2025-11-11 00:23:50 +0600

    Stage08 server: Implement db file config

---
+++ server/config/db.js
@@ -0,0 +1,34 @@
+// server/config/db.js
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri ="mongodb+srv://hero-a10-u01:LnbcvrnPHg9imLuI@cluster0.9busnhf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dbName = "studymateDB";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db, partnerCollection, requestCollection;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db(dbName);
+    partnerCollection = db.collection("partners");
+    requestCollection = db.collection("requests");
+    console.log("✅ MongoDB Connected Successfully!");
+  } catch (err) {
+    console.error("❌ MongoDB Connection Failed:", err);
+  }
+}
+
+function getCollections() {
+  if (!db) throw new Error("❌ Database not initialized!");
+  return { partnerCollection, requestCollection };
+}
+
+module.exports = { connectDB, getCollections, client };
